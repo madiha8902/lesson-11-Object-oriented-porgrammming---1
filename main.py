213d0dbbd890400ef43373ca3a1e12fba0659abd
madiha8902 <madiha.batool@bellfield.nsw.edu.au>
2024-10-24 19:39:10 +1100

    object-oriented programming in python part 1

---
+++ main.py
@@ -0,0 +1,18 @@
+# class student:
+#     grade=10
+#     name="madiha"
+#     def intro(self):
+#         print("hello I am",self.name)
+#     def details(self):
+#         print("hello I am",self.name,"and Iam in",self.grade)
+# obj=student()
+# obj.intro()
+# obj.details()
+
+class animals:
+    color="grey,white,black"
+    name="sugar glider"
+    def intro(self):
+        print("hello I am",self.name)
+    def details(self):
+        print("hello I am",self.name)
